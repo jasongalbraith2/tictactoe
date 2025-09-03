0f904389a805173ce8ba2cff895a59637320003d
U-BSD\463557 <463557@SHS-FYBF6X3.beaverton.k12.or.us>
2025-09-03 08:43:35 -0700

    transferring file

---
+++ tictactoe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void printBoard(const unsigned int (&positions)[3][3]) {
+  std::cout << " A | B | C \n         \n---|---|---\n " << positions[0][0];
+}
+  
+int main()
+{
+  unsigned char positions[3][3] = {' '};
+
+  bool running = true;
+  while (running) {
+      
+  }
+  
+  return 0;
+}
