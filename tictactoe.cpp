eb9fb66e15da6ecfbd06d2e1e6f3cab61ad4daac
U-BSD\463557 <463557@SHS-C6W2NW3.beaverton.k12.or.us>
2025-09-08 10:22:44 -0700

    Updated return function. Still needs to check for win correctly.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -7,72 +7,90 @@ const unsigned int COLS = 3;
 // Function to print out the board
 void printBoard(char** positions) {
   // Clear screen and print header
-  std::cout << "\033[H\033[J    A | B | C \n";
+  //std::cout << "\033[H\033[J    A | B | C \n";
 
   // Print rows
   for (int i = 0; i < ROWS; ++i) {
     std::cout << "   ---|---|---\n";
     std::cout << " " << i << "  " << positions[i][0] << " | " << positions[i][1] << " | " << positions[i][2] << "\n";
   }
-
+  
   // Add spacing for input text
   std::cout << "\n\n";
 }
 
+bool check_for_draw(char** positions) {
+  for (int i = 0; i < ROWS; ++i) {
+    for (int j = 0; j < COLS; ++j) {
+      if (positions[i][j] == ' ') { return false; }
+    }
+  }
+  return true;
+}
+
 bool check_for_win(char** positions) {
-  // Check for a horiztonal win
+  // Check for a horizontal win
   char initialChar;
   bool match = true;
 
   // Scan across each row and check if the initial character matches the next two.
   for (int i = 0; i < ROWS; ++i) {
     initialChar = positions[i][0];
-    for (int j = 1; j < COLS; ++j) {
-      if (initialChar != positions[i][j]) {
-	match = false;
+    if (initialChar != ' ') {
+      for (int j = 1; j < COLS; ++j) {
+	if (initialChar != positions[i][j]) {
+	  match = false;
+	}
       }
+      if (match) { return true; }
     }
-    if (match) { return true; }
   }
 
   // Scan across each column and check if the initial character matches the next two.
   match = true;
   for (int i = 0; i < COLS; ++i) {
     initialChar = positions[0][i];
-    for (int j = 0; j < ROWS; ++j) {
-      if (initialChar != positions[j][i]) {
-	match = false;
+    if (initialChar != ' ') {
+      for (int j = 0; j < ROWS; ++j) {
+	if (initialChar != positions[j][i]) {
+	  match = false;
+	}
       }
+      if (match) { return true; }
     }
-    if (match) { return true; }
   }
 
   // Scan across each diagonal..
   // Left up to right down (LU-RD)
   match = true;
   initialChar = positions[0][0];
-  for (int i = 1; i < ROWS; ++i) {
-    if (initialChar != positions[i][i]) {
-      match = false;
+  if (initialChar != ' ') {
+    for (int i = 1; i < ROWS; ++i) {
+      if (initialChar != positions[i][i]) {
+	match = false;
+      }
     }
+    if (match) { return true; }
   }
-  if (match) { return true; }
 
   // Right up to left down (RU-LD)
   match = true;
   initialChar = positions[ROWS - 1][0];
-  for (int i = ROWS - 2; i >= 0; --i) {
-    if (initialChar != positions[i][i]) {
-      match = false;
+  if (initialChar != ' ') {
+    for (int i = ROWS - 2; i >= 0; --i) {
+      if (initialChar != positions[i][i]) {
+	match = false;
+      }
     }
+    if (match) { return true; }
   }
-  if (match) { return true; }
-  else { return false; }
+
+  return false;
 }
 
 int main() {
   char** positions = new char*[ROWS];
-  char toPlace[2];
+  char toPlace[3];
 
   // Loop variables that infer results
   bool running = true;
@@ -110,28 +128,34 @@ int main() {
 
       // Lowercase the input, then use ascii conversion to check to see if it is within the range
       // Also, convert the row choice to an actual integer
-      columnChoice = static_cast<int>(std::tolower(toPlace[0])) - 97;
-      rowChoice = (int)toPlace[1];
+      columnChoice = std::tolower(toPlace[0]) - 'a';
+      rowChoice = toPlace[1] - '0';
       if (columnChoice > 2 or rowChoice > 2) {
 	std::cout << "\nInvalid column or row choice. Please try again. (press any key to continue)\n";
-	std::cin.ignore();
+	std::cin.ignore(10000, '\n');
+	std::cin.get();
       }
       else {
 	
 	// Now, check to see if the space is already taken
-	if (positions[rowChoice][columnChoice] == ' ') {
+	if (positions[rowChoice][columnChoice] != ' ') {
 	  std::cout << "\nYour selection is already filled, try another spot. (press any key to continue)\n";
-	  std::cin.ignore();
+	  std::cin.ignore(10000, '\n');
+	  std::cin.get();
 	}
 	else {
 
 	  // The space is open!! Now, assign the player's piece to this spot.
-	  positions[rowChoice][columnChoice] == (player1) ? 'X' : 'O';
-
+	  positions[rowChoice][columnChoice] = (player1) ? 'X' : 'O';
+	  
 	  // Check for win or draw
 	  if (check_for_win(positions)) {
 	    hasWon = true;
 	    printBoard(positions);
+
+	    // Add wins to the player
+	    if (player1) { ++player1Wins; }
+	    else { ++player2Wins; }
 
 	    std::cout << "CONGRATULATIONS TO " << ((player1) ? "PLAYER 1" : "PLAYER 2") << "!!!\n";
 	    std::cout << "Player 1 has " << player1Wins << " wins and Player 2 has " << player2Wins << "!\n";
